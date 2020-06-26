03cb3bfce731901a8d6bcc3197e572ae8ff4bef3
Travis Turner <RoboEnigma@gmail.com>
2020-06-26 17:25:43 +0300

    Updates

---
+++ precache-manifest.ebdda35831500be2c10d3b445dcd6b57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4293ae392d2f9b911379a9584b6b6169",
+    "url": "/emperor-sleep-student/index.html"
+  },
+  {
+    "revision": "72f587886d0a83a03201",
+    "url": "/emperor-sleep-student/static/css/2.4ea45184.chunk.css"
+  },
+  {
+    "revision": "fef1486afb8579fe6233",
+    "url": "/emperor-sleep-student/static/css/main.8e809a8a.chunk.css"
+  },
+  {
+    "revision": "72f587886d0a83a03201",
+    "url": "/emperor-sleep-student/static/js/2.23794e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emperor-sleep-student/static/js/2.23794e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef1486afb8579fe6233",
+    "url": "/emperor-sleep-student/static/js/main.2f30b2b5.chunk.js"
+  },
+  {
+    "revision": "69d4d905f5891adb62b6",
+    "url": "/emperor-sleep-student/static/js/runtime-main.59aa577e.js"
+  },
+  {
+    "revision": "97e22ea45ce550f7759caab955fc2591",
+    "url": "/emperor-sleep-student/static/media/HoltwoodOneSC-Regular.97e22ea4.ttf"
+  },
+  {
+    "revision": "837d1123022c5e9ec0febae5f5043ca0",
+    "url": "/emperor-sleep-student/static/media/holtwood-one-sc-latin-400.837d1123.woff2"
+  },
+  {
+    "revision": "f6b63d7e5b667afaaaf4e1bb0241da81",
+    "url": "/emperor-sleep-student/static/media/holtwood-one-sc-latin-400.f6b63d7e.woff"
+  }
+]);
